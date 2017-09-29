0205b7a65b70906308777f6c8b158c2989ac87fb
mustafa <devmustafasaeed@outlook.com>
2017-09-29 15:04:25 +0200

    setting up express

---
+++ app.js
@@ -0,0 +1,19 @@
+var experss = require('express');
+var app = experss();
+
+var port = 5000;
+
+app.use(experss.static('public'));
+app.use(experss.static('src/views'));
+
+app.get('/', function(req, res) {
+	res.send('hello world');
+});
+
+app.get('/books', function(req, res) {
+	res.send('hello books');
+});
+
+app.listen(port, function(err ) {
+	console.log('running server on port ' + port);
+});
