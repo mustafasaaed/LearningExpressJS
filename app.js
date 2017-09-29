4a3ecd6e2fe75abee3229c93ef29f70f60c3b577
mustafa <devmustafasaeed@outlook.com>
2017-09-29 16:46:48 +0200

    setting up gulp

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var experss = require('express');
 var app = experss();
 
-var port = 5000;
+var port = process.env.port || 5000;
 
 app.use(experss.static('public'));
 app.use(experss.static('src/views'));
 
-app.get('/', function(req, res) {
-	res.send('hello world');
+app.get('/', function (req, res) {
+    res.send('hello world');
 });
 
-app.get('/books', function(req, res) {
-	res.send('hello books');
+app.get('/books', function (req, res) {
+    res.send('hello books');
 });
 
-app.listen(port, function(err ) {
-	console.log('running server on port ' + port);
+app.listen(port, function (err) {
+    console.log('running server on port ' + port);
 });
---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var wiredep = require('wiredep').stream;
+var inject = require('gulp-inject');
+var nodemon = require('gulp-nodemon');
+
+var jsFiles = ['*.js', 'src/**/*.js'];
+
+gulp.task('style', function () {
+	return gulp.src(jsFiles)
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish', {
+			verbose: true
+		}))
+		.pipe(jscs());
+});
+
+var options = {
+	bowerJson: require('./bower.json'),
+	directory: './public/lib',
+	ignorePath: '../../public'
+};
+
+var injectSrc = gulp.src(['./public/css/*.css',
+	'./public/js/*.js'],
+	{
+		read: false
+	}
+);
+
+var injectOptions = {
+	ignorePath: '/public'
+};
+
+gulp.task('inject', function () {
+	return gulp.src('./src/views/*.html')
+		.pipe(wiredep(options))
+		.pipe(inject(injectSrc, injectOptions))
+		.pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve',['style', 'inject'], function() {
+	var options = {
+		script: 'app.js',
+		delayTime: 1,
+		env: {
+			'PORT': 3000
+		},
+		watch: jsFiles
+	};
+
+	return nodemon(options)
+	.on('restart', function(en) {
+		console.log('restarting....');
+	});
+});
